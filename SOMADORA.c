c73519bf7dc44d1bb8efb6135fe2fd71ada8f931
jvcalves2005-sudo <jvcalves2005@gmail.com>
2026-08-10 20:19:36 -0300

    Add files via upload

---
+++ SOMADORA.c
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+
+
+int main()
+{
+
+	int x, y, resultado;
+
+	printf("Coloque 2 numeros para realizar a soma: \n");
+
+	scanf("%i",&x);
+	scanf("%i",&y);
+
+	resultado = x + y;
+
+	printf("%i",resultado);
+
+}
