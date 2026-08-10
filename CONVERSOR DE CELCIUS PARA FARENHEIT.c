7f1ac3bca2946835ed47c64b697f40ee3ec58bb7
jvcalves2005-sudo <jvcalves2005@gmail.com>
2026-08-10 20:22:33 -0300

    Add files via upload

---
+++ CONVERSOR DE CELCIUS PARA FARENHEIT.c
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+
+
+int main()
+{
+
+    float C, F;
+    
+    printf("Coloque os graus Celcius: \n");
+    
+    scanf("%f", &C);
+    
+    F = C * 9/5 + 32;
+    
+    printf("%.2f", F);
+
+}
